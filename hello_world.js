582731e555f9203bc6f77a09c3acde3473905c8b
wangxu <wx971025@163.com>
2026-03-02 09:45:09 +0000

    init commit

--- hello_world.js
+++ hello_world.js
@@ -3,5 +3,3 @@ console.log('Hello, Node.js!');
 console.log('Node.js 版本:', process.version);
 console.log('当前工作目录:', process.cwd());
 console.log('操作系统:', process.platform);
-
-console.log(process.argv.slice(2));
